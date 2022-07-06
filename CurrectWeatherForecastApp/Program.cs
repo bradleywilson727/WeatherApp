6704ea95aeac4d66b90b38554823ce305bec0349
Bradley Wilson <bradleywilson727@gmail.com>
2022-07-06 10:12:55 -0500

    completed assignment

---
+++ CurrectWeatherForecastApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace CurrectWeatherForecastApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = File.ReadAllText("appsettings.json");
+            var apiKey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+            var client = new HttpClient();
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip=35040,us&appid={apiKey}&units=imperial";
+            var weatherResult = client.GetStringAsync(weatherURL).Result;
+            var weatherDisplay = JObject.Parse(weatherResult)["main"].ToString();
+
+            Console.WriteLine(weatherDisplay);
+        }
+    }
+}
